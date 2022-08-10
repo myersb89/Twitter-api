bd8b38947b537e143d9ab5490eacad1598d3f274
Bryan J Myers <myersb89@gmail.com>
2022-08-10 11:50:02 -0500

    test out tweepy

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import requests
 import os
 import json
 import psycopg2
+import tweepy
 
-
-def main():
+def get_user_tweets_native():
     baseurl = "https://api.twitter.com/2"
     access_token = os.getenv('BearerToken')
     headers = {'Authorization': f'Bearer {access_token}'}
@@ -12,15 +12,12 @@ def main():
     query_params = {'user.fields': "pinned_tweet_id"}
     username = "BryanJMyers1"
 
-    conn = psycopg2.connect("dbname=twitter_data user=postgres password=test1234")
 
     try:
         r = requests.get(baseurl + f'/users/by/username/{username}', headers=headers, params=query_params)
         r.raise_for_status()
     except requests.exceptions.HTTPError as err:
         raise SystemExit(err)
-
-    print(json.dumps(r.json()))
 
     pinned_tweet = r.json()['data']['pinned_tweet_id']
     ids = {'ids':pinned_tweet}
@@ -31,9 +28,18 @@ def main():
         raise SystemExit(err)
 
     print(json.dumps(r.json()))
-    #query_params = {'query': '(from:twitterdev -is:retweet) OR #twitterdev','tweet.fields': 'author_id'}
 
-    conn.close()
+def get_user_tweets_library():
+    client = tweepy.Client(bearer_token=os.getenv('BearerToken'))
+
+    response = client.get_user(username="BryanJMyers1")
+
+    public_tweets = client.get_users_tweets(response.data.id)
+    for tweet in public_tweets.data:
+        print(tweet.text)
 
 if __name__ == "__main__":
-    main()
+    conn = psycopg2.connect("dbname=twitter_data user=postgres password=test1234")
+    get_user_tweets_native()
+    get_user_tweets_library()
+    conn.close()

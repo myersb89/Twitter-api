67f775843e023406c46f89ca211ae7fb4f07d083
Bryan J Myers <myersb89@gmail.com>
2022-08-10 14:35:08 -0500

    save to postgres

--- main.py
+++ main.py
@@ -40,16 +40,55 @@ def get_user_tweets_library(user):
     for tweet in public_tweets.data:
         print(tweet.text)
 
+    return public_tweets.data
+
 def stream_tweets():
     stream_client = TweetPrinter(bearer_token=os.getenv('BearerToken'))
     stream_client.add_rules(tweepy.StreamRule("Tweepy"))
     stream_client.filter()
 
-if __name__ == "__main__":
-    conn = psycopg2.connect("dbname=twitter_data user=postgres password=test1234")
+def create_tables(connection):
+    commands = (
+        """
+        CREATE TABLE IF NOT EXISTS tweets (
+            tweet_id BIGINT PRIMARY KEY,
+            tweet_text VARCHAR(280) NOT NULL
+        )
+        """,
+    )
+   # import pdb; pdb.set_trace()
+    cur = connection.cursor()
+    for command in commands:
+        cur.execute(command)
+    cur.close()
+    connection.commit()
 
+def insert_tweet_sql(connection, tweet):
+    cur = connection.cursor()
+    cur.execute("INSERT INTO tweets (tweet_id, tweet_text) VALUES (%s, %s) ON CONFLICT DO NOTHING", (tweet.id, tweet.text))
+    cur.close()
+    connection.commit()
+
+def get_tweets_sql(connection):
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM tweets;")
+    tweets = cur.fetchall()
+    for tweet in tweets:
+        print(tweet)
+    cur.close()
+
+if __name__ == "__main__":
     username = "BryanJMyers1"
     #get_user_tweets_native(username)
-    #get_user_tweets_library(username)
+    tweets = get_user_tweets_library(username)
+    print(tweets)
+
+    # Save to Postgres
+    conn = psycopg2.connect("dbname=twitter_data user=postgres password=test1234")
+    create_tables(conn)
+    for tweet in tweets:
+        insert_tweet_sql(conn,tweet)
+    get_tweets_sql(conn)
     conn.close()
-    stream_tweets()
+
+    #stream_tweets()
